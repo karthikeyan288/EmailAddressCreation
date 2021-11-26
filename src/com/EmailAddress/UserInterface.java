c56a8dca0fa9a176d752e4cf572f61f80fb1498d
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-26 06:47:23 +0530

    ADDED : getting the user information

---
+++ src/com/EmailAddress/UserInterface.java
@@ -0,0 +1,34 @@
+package com.EmailAddress;
+
+public class UserInterface {
+	private String emailAddressUc1;
+	private String emailAddressUc2;
+	private String emailAddressUc3;
+	private String emailAddressUc4;
+	private String emailAddressUc5;
+
+	public static void main(String[] args) {
+		UserInterface userInterface = new UserInterface();
+		EmailPattern emailPattern = new EmailPattern();
+
+		emailPattern.getemailAddressUc1();
+		userInterface.emailAddressUc1 = emailPattern.getEmailAddressUc1();
+		emailPattern.getemailAddressUc2();
+		userInterface.emailAddressUc2 = emailPattern.getEmailAddressUc2();
+		emailPattern.getemailAddressUc3();
+		userInterface.emailAddressUc3 = emailPattern.getEmailAddressUc3();
+		emailPattern.getemailAddressUc4();
+		userInterface.emailAddressUc4 = emailPattern.getEmailAddressUc4();
+		emailPattern.getemailAddressUc5();
+		userInterface.emailAddressUc5 = emailPattern.getEmailAddressUc5();
+
+		userInterface.display();
+	}
+
+	private void display() {
+		System.out.println("\nThe email for condition 1:" + emailAddressUc1 + "\nThe email for condition 2:"
+				+ emailAddressUc2 + "\nThe email for condition 3:" + emailAddressUc3 + "\nThe email for condition 4 "
+				+ emailAddressUc4 + "\\nThe email for condition 5:" + emailAddressUc5);
+	}
+
+}
