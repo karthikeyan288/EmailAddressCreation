bbe62c284f888936102e17e0401d494447b1675b
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-26 06:29:09 +0530

    ADDED : getthing pattern for email address rule 4

---
+++ src/com/EmailAddress/EmailPattern.java
@@ -0,0 +1,93 @@
+package com.EmailAddress;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class EmailPattern {
+	private String emailAddressUc1;
+	private String emailAddressUc2;
+	private String emailAddressUc3;
+	private String emailAddressUc4;
+
+	Scanner sc = new Scanner(System.in);
+
+	public void getemailAddressUc1() {
+		System.out.println("Enter the emailaddtress for condition :1");
+		emailAddressUc1 = sc.nextLine();
+		boolean b3 = Pattern.matches("^[a-z]+[a-b]*$", emailAddressUc1);
+		if (b3) {
+			System.out.println(b3 + "->" + " Match found for condition-1");
+		} else {
+			System.out.println(b3 + "->" + "Match not found condition-1");
+			getemailAddressUc1();
+		}
+	}
+
+	public void getemailAddressUc2() {
+		System.out.println("Enter the emailaddtress for condition :2");
+		emailAddressUc2 = sc.nextLine();
+		boolean b3 = Pattern.matches("^[a-z]+[a-b]*@[a-z]+$", emailAddressUc2);
+		if (b3) {
+			System.out.println(b3 + "->" + " Match found for condition-2");
+		} else {
+			System.out.println(b3 + "->" + "Match not found condition-2");
+			getemailAddressUc2();
+		}
+	}
+	
+	public void getemailAddressUc3() {
+		System.out.println("Enter the emailaddtress for condition :3");
+		emailAddressUc3 = sc.nextLine();
+		boolean b3 = Pattern.matches("^[a-z.]+[a-b]*@[a-z]+.([a-z]{2})+$", emailAddressUc3);
+		if (b3) {
+			System.out.println(b3 + "->" + " Match found for condition-3");
+		} else {
+			System.out.println(b3 + "->" + "Match not found condition-3");
+			getemailAddressUc3();
+		}
+	}
+	
+	public void getemailAddressUc4() {
+		System.out.println("Enter the emailaddtress for condition :4");
+		emailAddressUc4 = sc.nextLine();
+		boolean b3 = Pattern.matches("^[a-z.]+[a-b]*@[a-z]+.([a-z]{2,})+[.-_+a-z]*$", emailAddressUc4);
+		if (b3) {
+			System.out.println(b3 + "->" + " Match found for condition-4");
+		} else {
+			System.out.println(b3 + "->" + "Match not found condition-4");
+			getemailAddressUc3();
+		}
+	}
+
+	public String getEmailAddressUc1() {
+		return emailAddressUc1;
+	}
+
+	public void setEmailAddressUc1(String emailAddressUc1) {
+		this.emailAddressUc1 = emailAddressUc1;
+	}
+
+	public String getEmailAddressUc2() {
+		return emailAddressUc2;
+	}
+
+	public void setEmailAddressUc2(String emailAddressUc2) {
+		this.emailAddressUc2 = emailAddressUc2;
+	}
+
+	public String getEmailAddressUc3() {
+		return emailAddressUc3;
+	}
+
+	public void setEmailAddressUc3(String emailAddressUc3) {
+		this.emailAddressUc3 = emailAddressUc3;
+	}
+
+	public String getEmailAddressUc4() {
+		return emailAddressUc4;
+	}
+
+	public void setEmailAddressUc4(String emailAddressUc4) {
+		this.emailAddressUc4 = emailAddressUc4;
+	}
+}
